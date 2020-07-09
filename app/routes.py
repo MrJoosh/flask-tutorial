4a0df8f83430ec47a53071f6bef78a7869a9ff9c
Josh Jones <mrjoosh@pm.me>
2020-07-09 20:19:53 +0100

    Extend login functionality

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 
 from app import webapp
 from app.forms import LoginForm
@@ -21,7 +21,15 @@ def index():
     return render_template('index.html', title='Home', user=user, posts=posts)
 
 
-@webapp.route('/login')
+@webapp.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash(
+            'Login requested for user {}, remember_me={}'.format(
+                form.username.data,
+                form.remember_me.data
+            )
+        )
+        return redirect('/index')
     return render_template('login.html', title='Sign In', form=form)
