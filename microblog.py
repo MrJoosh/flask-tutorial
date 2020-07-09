8d6cfc532c5a61686daf3c3cefb5ec36f6bdf5cf
Josh Jones <mrjoosh@pm.me>
2020-07-09 21:36:40 +0100

    extend flask shell to give db context

--- microblog.py
+++ microblog.py
@@ -1,2 +1,6 @@
+from app import webapp, db
+from app.models import User, Post
 
-from app import webapp
+@webapp.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
