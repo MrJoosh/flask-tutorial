a0b4d269b83e8cb09a5e729de6189fd4c820a2b3
Josh Jones <mrjoosh@pm.me>
2020-07-10 10:19:09 +0100

    Fix user password check method name

--- app/models.py
+++ app/models.py
@@ -17,7 +17,7 @@ class User(UserMixin, db.Model):
     
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
-    def check_password(self, password):
+    def check_password_hash(self, password):
         return check_password_hash(self.password_hash, password)
 
 
