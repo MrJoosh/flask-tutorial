223b47ced2b26ef9ceca254a49d1c643d6254776
Josh Jones <mrjoosh@pm.me>
2020-07-10 10:52:49 +0100

    Add user registration page

--- app/routes.py
+++ app/routes.py
@@ -3,7 +3,7 @@ from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
 
 from app import webapp
-from app.forms import LoginForm
+from app.forms import LoginForm, RegistrationForm
 from app.models import User
 
 
@@ -46,3 +46,18 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+
+@webapp.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
