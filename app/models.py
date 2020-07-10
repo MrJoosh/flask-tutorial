b9c3362e663853f5daa86ddfce4b6e047f354d21
Josh Jones <mrjoosh@pm.me>
2020-07-10 11:27:18 +0100

    Add user avatar support using Gravatar

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from hashlib import md5
 
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
@@ -17,8 +18,16 @@ class User(UserMixin, db.Model):
     
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
+    
     def check_password_hash(self, password):
         return check_password_hash(self.password_hash, password)
+    
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
+            digest,
+            size
+        )
 
 
 class Post(db.Model):
