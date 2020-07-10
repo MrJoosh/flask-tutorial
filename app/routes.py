232e1a4b28d37cc49ce28d30b1f265c1c000df51
Josh Jones <mrjoosh@pm.me>
2020-07-10 11:53:07 +0100

    Add about_me and last_seen to profile; implement last_seen update

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
@@ -5,6 +7,13 @@ from werkzeug.urls import url_parse
 from app import webapp, db
 from app.forms import LoginForm, RegistrationForm
 from app.models import User
+
+
+@webapp.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()  # pylint: disable=no-member
 
 
 @webapp.route('/')
