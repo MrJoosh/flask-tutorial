5c02ed6c90b58c7fb7f528009c583f8f9eeb9ed6
Josh Jones <mrjoosh@pm.me>
2020-07-10 10:55:30 +0100

    Add db into scope

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@ from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
 
-from app import webapp
+from app import webapp, db
 from app.forms import LoginForm, RegistrationForm
 from app.models import User
 
