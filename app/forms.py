09b6b970cd10ba4e7ee0f349660e92c0fa22b6ca
Josh Jones <mrjoosh@pm.me>
2020-07-11 13:36:29 +0100

    Add validation to username in profile edit

--- app/forms.py
+++ app/forms.py
@@ -33,3 +33,13 @@ class EditProfileForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
     submit = SubmitField('Submit')
+
+    def __init__(self, original_username, *args, **kwargs):
+        self.original_username = original_username
+        super(EditProfileForm, self).__init__(*args, **kwargs)
+
+    def validate_username(self, username):
+        if username.data != self.original_username:
+            user = User.query.filter_by(username=self.username.data).first()
+            if user is not None:
+                raise ValidationError('Username is not available - please use a different username')
