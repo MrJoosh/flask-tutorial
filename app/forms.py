1cd05789da39142a6c835fb73bb52656e9480fa8
Josh Jones <mrjoosh@pm.me>
2020-07-11 15:51:05 +0100

    Add follow/unfollow functionality to user pages

--- app/forms.py
+++ app/forms.py
@@ -43,3 +43,7 @@ class EditProfileForm(FlaskForm):
             user = User.query.filter_by(username=self.username.data).first()
             if user is not None:
                 raise ValidationError('Username is not available - please use a different username')
+
+
+class EmptyForm(FlaskForm):
+    submit = SubmitField('Submit')
--- app/routes.py
+++ app/routes.py
@@ -5,7 +5,7 @@ from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
 
 from app import webapp, db
-from app.forms import LoginForm, RegistrationForm, EditProfileForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm, EmptyForm
 from app.models import User
 
 
@@ -80,7 +80,8 @@ def user(username):
         {'author': user, 'body': 'Test post #1'},
         {'author': user, 'body': 'Test post #2'}
     ]
-    return render_template('user.html', user=user, posts=posts)
+    form = EmptyForm()
+    return render_template('user.html', user=user, posts=posts, form=form)
 
 
 @webapp.route('/edit_profile', methods=['GET', 'POST'])
@@ -97,3 +98,43 @@ def edit_profile():
         form.username.data = current_user.username
         form.about_me.data = current_user.about_me
     return render_template('edit_profile.html', title='Edit Profile', form=form)
+
+
+@webapp.route('/follow/<username>', methods=['POST'])
+@login_required
+def follow(username):
+    form = EmptyForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=username).first()
+        if user is None:
+            flash('User {} not found.'.format(username))
+            return redirect(url_for('index'))
+        if user == current_user:
+            flash("You can't follow yourself!")
+            return redirect(url_for('user', username=username))
+        current_user.follow(user)
+        db.session.commit()  # pylint: disable=no-member
+        flash('You are now following {}!'.format(username))
+        return redirect(url_for('user', username=username))
+    else:
+        return redirect(url_for('index'))
+
+
+@webapp.route('/unfollow/<username>', methods=['POST'])
+@login_required
+def unfollow(username):
+    form = EmptyForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=username).first()
+        if user is None:
+            flash('User {} not found.'.format(username))
+            return redirect(url_for('index'))
+        if user == current_user:
+            flash("You can't unfollow yourself!")
+            return redirect(url_for('user', username=username))
+        current_user.unfollow(user)
+        db.session.commit()  # pylint: disable=no-member
+        flash('You are no longer following {}!'.format(username))
+        return redirect(url_for('user', username=username))
+    else:
+        return redirect(url_for('index'))
