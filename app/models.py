84e8f3d6bc315166750095595f3ee1bdec63eb0f
Josh Jones <mrjoosh@pm.me>
2020-07-11 15:06:39 +0100

    Add method to user model to pull followed posts

--- app/models.py
+++ app/models.py
@@ -56,6 +56,16 @@ class User(UserMixin, db.Model):
     
     def is_following(self, user):
         return self.followed.filter(followers.c.followed_id == user.id).count() > 0
+    
+    def followed_posts(self):
+        return Post.query.join(
+            followers,
+            (followers.c.followed_id == Post.user.id)
+        ).filter(
+            followers.c.follower_id == self.id
+        ).order_by(
+            Post.timestamp.desc()
+        )
 
 
 class Post(db.Model):
