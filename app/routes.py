997aa5b461c9675f27a1cf6833a49f6e7d31d68c
Josh Jones <mrjoosh@pm.me>
2020-07-11 16:20:50 +0100

    Implement an explore page; Use sub template in index

--- app/routes.py
+++ app/routes.py
@@ -136,3 +136,10 @@ def unfollow(username):
         return redirect(url_for('user', username=username))
     else:
         return redirect(url_for('index'))
+
+
+@webapp.route('/explore')
+@login_required
+def explore():
+    posts = Post.query.order_by(Post.timestamp.desc()).all()
+    return render_template('index.html', title='Explore', posts=posts)
