4c57333dbf81436a3d2a681478f42a090bf3fe5f
Josh Jones <mrjoosh@pm.me>
2020-07-11 14:34:23 +0100

    Add capability for followers to db models

--- app/models.py
+++ app/models.py
@@ -6,6 +6,13 @@ from flask_login import UserMixin
 
 from app import db, login
 
+
+followers = db.Table(  # pylint: disable=no-member
+    'followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),  # pylint: disable=no-member
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))  # pylint: disable=no-member
+)
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)  # pylint: disable=no-member
     username = db.Column(db.String(64), index=True, unique=True)  # pylint: disable=no-member
@@ -14,6 +21,14 @@ class User(UserMixin, db.Model):
     about_me = db.Column(db.String(140))  # pylint: disable=no-member
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)  # pylint: disable=no-member
     posts = db.relationship('Post', backref='author', lazy='dynamic')  # pylint: disable=no-member
+    followed = db.relationship(  # pylint: disable=no-member
+        'User',
+        secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        backref=db.backref('followers', lazy='dynamic'),  # pylint: disable=no-member
+        lazy='dynamic'
+    )
 
     def __repr__(self):
         return "<User {}>".format(self.username)
@@ -30,6 +45,17 @@ class User(UserMixin, db.Model):
             digest,
             size
         )
+
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
+    
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+    
+    def is_following(self, user):
+        return self.followed.filter(followers.c.followed_id == user.id).count() > 0
 
 
 class Post(db.Model):
