415b48bd1a6401d2a2aba326e8c545b5b5bfb372
Josh Jones <mrjoosh@pm.me>
2020-07-11 16:46:11 +0100

    Remove mocked posts from user page; Add real logic and pagination

--- config.py
+++ config.py
@@ -11,4 +11,4 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['mrjoosh@pm.me']
-    POSTS_PER_PAGE = 3
+    POSTS_PER_PAGE = 25
