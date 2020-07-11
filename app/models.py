e33310ce0fc424f6440ae67bf44e7d30ac2da4c5
Josh Jones <mrjoosh@pm.me>
2020-07-11 15:09:15 +0100

    Change followed posts method to include own posts

--- app/models.py
+++ app/models.py
@@ -58,14 +58,14 @@ class User(UserMixin, db.Model):
         return self.followed.filter(followers.c.followed_id == user.id).count() > 0
     
     def followed_posts(self):
-        return Post.query.join(
+        followed = Post.query.join(
             followers,
             (followers.c.followed_id == Post.user.id)
         ).filter(
             followers.c.follower_id == self.id
-        ).order_by(
-            Post.timestamp.desc()
         )
+        own = Post.query.filter_by(user_id=self.id)
+        return followed.union(own).order_by(Post.timestamp.desc())
 
 
 class Post(db.Model):
