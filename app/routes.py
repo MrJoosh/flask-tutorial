c1dc8718e88d18021fa2941f9001471e129e6635
Josh Jones <mrjoosh@pm.me>
2020-07-11 16:40:15 +0100

    Add page control to index/explore pages

--- app/routes.py
+++ app/routes.py
@@ -29,7 +29,36 @@ def index():
         return redirect(url_for('index'))
     page = request.args.get('page', 1, type=int)
     posts = current_user.followed_posts().paginate(page, webapp.config['POSTS_PER_PAGE'], False)
-    return render_template('index.html', title='Home', posts=posts.items, form=form)
+    next_url = url_for('index', page=posts.next_num) if posts.has_next else None
+    prev_url = url_for('index', page=posts.prev_num) if posts.has_prev else None
+    return render_template(
+        'index.html',
+        title='Home',
+        posts=posts.items,
+        form=form,
+        next_url=next_url,
+        prev_url=prev_url
+    )
+
+
+@webapp.route('/explore')
+@login_required
+def explore():
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.timestamp.desc()).paginate(
+        page,
+        webapp.config['POSTS_PER_PAGE'],
+        False
+    )
+    next_url = url_for('explore', page=posts.next_num) if posts.has_next else None
+    prev_url = url_for('explore', page=posts.prev_num) if posts.has_prev else None
+    return render_template(
+        'index.html',
+        title='Explore',
+        posts=posts.items,
+        next_url=next_url,
+        prev_url=prev_url
+    )
 
 
 @webapp.route('/login', methods=['GET', 'POST'])
@@ -137,11 +166,3 @@ def unfollow(username):
         return redirect(url_for('user', username=username))
     else:
         return redirect(url_for('index'))
-
-
-@webapp.route('/explore')
-@login_required
-def explore():
-    page = request.args.get('page', 1, type=int)
-    posts = Post.query.order_by(Post.timestamp.desc()).paginate(page, webapp.config['POSTS_PER_PAGE'], False)
-    return render_template('index.html', title='Explore', posts=posts.items)
