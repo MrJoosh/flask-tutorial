d284c5b9d87c93092472f864fdfc1a8667b1ee16
Josh Jones <mrjoosh@pm.me>
2020-07-13 19:22:20 +0100

    Implement moment library for date/time formatting where required

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,7 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 from config import Config
 
@@ -19,6 +20,7 @@ login = LoginManager(webapp)
 login.login_view = 'login'
 mail = Mail(webapp)
 bootstrap = Bootstrap(webapp)
+moment = Moment(webapp)
 
 from app import routes, models, errors
 
