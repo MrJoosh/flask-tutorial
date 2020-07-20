aca7d38fce0d0f9f9f98899dca3460b270eba83c
Josh Jones <mrjoosh@pm.me>
2020-07-20 19:31:58 +0100

    Integrate elasticsearch functionality with sql alchemy

--- app/models.py
+++ app/models.py
@@ -8,6 +8,7 @@ from flask import current_app
 from flask_login import UserMixin
 
 from app import db, login
+from app.search import add_to_index, remove_from_index, query_index
 
 
 followers = db.Table(  # pylint: disable=no-member
@@ -15,6 +16,49 @@ followers = db.Table(  # pylint: disable=no-member
     db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),  # pylint: disable=no-member
     db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))  # pylint: disable=no-member
 )
+
+
+class SearchableMixin(object):
+    @classmethod
+    def search(cls, expression, page, per_page):
+        ids, total = query_index(cls.__tablename__, expression, page, per_page)
+        if total == 0:
+            return cls.query.filter_by(id=0), 0
+        when = []
+        for i in range(len(ids)):
+            when.append((ids[i], i))
+        return cls.query.filter(cls.id.in_(ids)).order_by(db.case(when, value=cls.id)), total
+    
+    @classmethod
+    def before_commit(cls, session):
+        session._changes = {
+            'add': list(session.new),
+            'update': list(session.dirty),
+            'delete': list(session.deleted)
+        }
+    
+    @classmethod
+    def after_commit(cls, session):
+        for obj in session._changes['add']:
+            if isinstance(obj, SearchableMixin):
+                add_to_index(obj.__tablename__, obj)
+        for obj in session._changes['update']:
+            if isinstance(obj, SearchableMixin):
+                add_to_index(obj.__tablename__, obj)
+        for obj in session._changes['delete']:
+            if isinstance(obj, SearchableMixin):
+                remove_from_index(obj.__tablename__, obj)
+        session._changes = None
+    
+    @classmethod
+    def reindex(cls):
+        for obj in cls.query:
+            add_to_index(cls.__tablename__, obj)
+
+
+db.event.listen(db.session, 'before_commit', SearchableMixin.before_commit)
+db.event.listen(db.session, 'after_commit', SearchableMixin.after_commit)
+
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)  # pylint: disable=no-member
@@ -93,7 +137,7 @@ class User(UserMixin, db.Model):
         return User.query.get(id)
 
 
-class Post(db.Model):
+class Post(SearchableMixin, db.Model):
     __searchable__ = ['body']
     id = db.Column(db.Integer, primary_key=True)  # pylint: disable=no-member
     body = db.Column(db.String(140))  # pylint: disable=no-member
